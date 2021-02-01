be7baf4522704ab571dbfceb5a52c023e69c4dec
vilrodriguez (via Travis CI) <vilmango@hotmail.com>
2021-02-01 08:21:48 +0000

    Deploy Release

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElement = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', (event)=>{
   console.log('open hamburger');
   event.preventDefault();
   if(header.classList.contains('open')){ // Close ham menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElement.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
   } else { // Open ham menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.add('fade-in');
-    overlay.classList.remove('fade-out');
+    fadeElement.forEach(function(element){
+      element.classList.add('fade-in');
+      element.classList.remove('fade-out');
+    })
   }
 });
