855d96dc988224cb45ac01a7ff78bf717bcf1a64
vilrodriguez (via Travis CI) <vilmango@hotmail.com>
2021-02-01 05:47:01 +0000

    Deploy Release

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-console.log('Hello u  w u');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', (event)=>{
+  console.log('open hamburger');
+  event.preventDefault();
+  if(header.classList.contains('open')){
+    header.classList.remove('open');
+  } else {
+    header.classList.add('open');
+  }
+});
