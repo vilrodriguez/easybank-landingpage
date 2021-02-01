4d59a861babe522e1bb756a89587421ed3f108d6
vilrodriguez <vilmango@hotmail.com>
2021-02-01 01:59:06 -0500

    added animation to the overlay

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', (event)=>{
   console.log('open hamburger');
   event.preventDefault();
-  if(header.classList.contains('open')){
+  if(header.classList.contains('open')){ // Close ham menu
     header.classList.remove('open');
-  } else {
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else { // Open ham menu
     header.classList.add('open');
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
   }
 });
